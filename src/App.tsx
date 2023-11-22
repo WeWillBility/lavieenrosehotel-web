c1ff5e26c0dbd65d52c61437cce590f034ac6389
WonJun <c68254@gmail.com>
2023-11-22 19:19:49 +0900

    Revert "✨ feat: 관리자용 사이드바 디테일 컴포넌트 추가"

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import SidebarAdmin from './components/Sidebar/SidebarAdmin'
 import SidebarUser from './components/Sidebar/SidebarUser'
 import TopBar from './components/TopBar/TopBar'
 import BodyTitle from './components/BodyTitle/BodyTitle'
-import AdminSidebarDetail from './components/AdminSidebarDetail/AdminSiderbarDetail'
 import './App.css'
 
 function App() {
@@ -13,9 +12,8 @@ function App() {
     <>
       <TopBar pageName='페이지 이름'/>
       {/* <SidebarUser /> */}
-      {/* <SidebarAdmin /> */}
-      <AdminSidebarDetail adminSidebarName='액티비티 정보' />
-      {/* <BodyTitle bodyName='마이페이지'/> */}
+      <SidebarAdmin />
+      <BodyTitle bodyName='마이페이지'/>
     </>
   )
 }
